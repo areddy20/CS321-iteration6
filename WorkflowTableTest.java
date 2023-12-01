f4c3a10ee26b9df9006dd65914af954dcbfef584
areddy20 <89561629+areddy20@users.noreply.github.com>
2023-12-01 16:18:45 -0500

    Create WorkflowTableTest.java

---
+++ WorkflowTableTest.java
@@ -0,0 +1,52 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class WorkflowTableTest {
+
+    /**
+     * Testing getCurrentStep().
+     * Should pass since "Done!" = "Done!".
+     */
+    @Test
+    public void testInitialStep() {
+        
+        WorkflowTable workflow = new WorkflowTable();
+        
+        assertEquals("Done!", workflow.getCurrentStep());  
+    
+    }
+
+    /**
+     * Testing loadNextItem(). 
+     * Should fail since "Loading next item now" != "Loading the first item now".
+     * 
+     * UPDATE: should now pass
+     */
+    @Test
+    public void testLoadNextItem() {
+        
+        WorkflowTable workflow = new WorkflowTable();
+        
+        workflow.loadNextItem();
+        
+        assertEquals("Loading first item now", workflow.getCurrentStep());  
+    
+    }
+
+    /**
+     * Testing updateStep().
+     * Should pass since "Step is updated" = "Step is updated".
+     */
+    @Test
+    public void testUpdateStep() {
+        
+        WorkflowTable workflow = new WorkflowTable();
+        
+        workflow.updateStep("Step is updated");
+        
+        assertEquals("Step is updated", workflow.getCurrentStep());  
+    
+    }
+
+}
+
